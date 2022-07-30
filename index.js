cc2a049cd7532db8054ff798a504a3cd892162eb
Baesee <anekddl7@naver.com>
2022-07-31 02:31:20 +0900

    form 데이터 전송 ( use body-parser )

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+app.use(express.urlencoded({extended: true})) 
+
 
 app.listen(8080, function(){
     console.log(" listening on 8080 , 8080으로 오픈");
@@ -17,3 +19,12 @@ app.get('/beauty', function(req, res){
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
+
+app.get('/write', function(req, res){
+    res.sendFile(__dirname + '/write.html');
+});
+
+app.post('/add', function(req, res){
+    res.send('전송 성공');
+    console.log(req.body.title);
+})
